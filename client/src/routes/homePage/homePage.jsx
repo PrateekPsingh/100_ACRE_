e485fec5e1c897d35a86a3996a2b2d442c638134
PrateekPsingh <Prateeksingh5116548@gmail.com>
2024-07-03 00:10:16 +0530

    Adding frontend ,api and socket folder

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Unlock your perfect stay or investment with ease.</h1>
+          <p>
+          Discover your ideal property or perfect hotel stay effortlessly with [Website Name]. Browse curated listings and exclusive rentals tailored to your lifestyle. From luxurious homes to memorable getaways, find what suits you best. Start your journey to exceptional living and unforgettable experiences today.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
