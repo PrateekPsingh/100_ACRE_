b1cc589ad588a49c119451db1688a684cfbce435
PrateekPsingh <Prateeksingh5116548@gmail.com>
2024-09-12 06:18:21 +0530

    New Endpoint

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://100-acre-8izx-8t1wkk0co-prateekpsinghs-projects.vercel.app/",
+  baseURL: "https://100-acre-8izx.vercel.app/",
   withCredentials: true,
 });
 
