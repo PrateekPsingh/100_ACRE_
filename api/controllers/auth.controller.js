fec69f526eab717f4822e6787b490a8bb5c3224d
Prateek Pratap Singh <Prateeksingh5116548@gmail.com>
2024-10-10 18:56:22 +0530

    Update auth.controller.js for Production Mode

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -68,8 +68,9 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
+        secure:true,
         maxAge: age,
+        sameSite: 'none',
       })
       .status(200)
       .json(userInfo);
