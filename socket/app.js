cf6bff7b01d364500e476995f76b5c4ec2d1cdfa
Prateek Pratap Singh <Prateeksingh5116548@gmail.com>
2024-11-13 02:28:09 +0530

    Update app.js for CORS policy

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "https://100-acre-z5d7.vercel.app/",
+    origin: "https://100-acre-z5d7.vercel.app",
   },
 });
 
